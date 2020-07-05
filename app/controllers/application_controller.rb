3817e981ca48c60a948c6423dab3afc9679b7fde
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-07-05 14:41:35 -0400

    Creates Show route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,15 @@ class ApplicationController < Sinatra::Base
     get '/books' do
       @books = Book.all
       erb :'books/index'
+    end
 
+    get '/books/:id' do
+      @book = Book.find_by_id(params[:id])
+
+      if @book
+        erb :'/books/show'
+      else
+        redirect '/books'
+      end
     end
 end
