9356e1911fec1e49f7f3f9b9c3be92a9f65128de
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-07-05 17:40:08 -0400

    reformats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,49 +11,49 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-    get '/books' do
-      @books = Book.all
-      erb :'books/index'
+  get '/books' do
+    @books = Book.all
+    erb :'books/index'
+  end
+
+  get '/books/new' do
+    @users = User.all
+    erb :'/books/new'
+  end
+
+  post '/books' do
+    user = User.find_by_id(params[:user_id])
+    book = user.books.build(params)
+
+    if book.save
+      redirect '/books'
+    else
+      redirect '/books/new'
     end
+  end
 
-    get '/books/new' do
-      @users = User.all
-      erb :'/books/new'
+  get '/books/:id' do
+    @book = Book.find_by_id(params[:id])
+
+    if @book
+      erb :'/books/show'
+    else
+      redirect '/books'
     end
+  end
 
-    post '/books' do
-      user = User.find_by_id(params[:user_id])
-      book = user.books.build(params)
+  get '/books/:id/edit' do
+    @book = Book.find_by_id(params[:id])
+    erb :'/books/edit'
+  end
 
-      if book.save
-        redirect '/books'
-      else
-        redirect '/books/new'
-      end
+  patch '/books/:id' do
+    book = Book.find_by_id(params[:id])
+    if book.update(title: params[:title], author: params[:author])
+      redirect "/books/#{book.id}"
+    else
+      redirect '/books/new'
     end
-
-    get '/books/:id' do
-      @book = Book.find_by_id(params[:id])
-
-      if @book
-        erb :'/books/show'
-      else
-        redirect '/books'
-      end
-    end
-
-    get '/books/:id/edit' do
-      @book = Book.find_by_id(params[:id])
-      erb :'/books/edit'
-    end
-
-    patch '/books/:id' do
-      book = Book.find_by_id(params[:id])
-      if book.update(title: params[:title], author: params[:author])
-        redirect "/books/#{book.id}"
-      else
-        redirect '/books/new'
-      end
-    end
+  end
 
 end
