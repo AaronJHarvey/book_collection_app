1e2590132e6fb640ae478ab5852b6791b3836038
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-07-07 20:19:38 -0400

    Adds password column to users table, adds password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200703190530) do
+ActiveRecord::Schema.define(version: 20200708001436) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20200703190530) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
